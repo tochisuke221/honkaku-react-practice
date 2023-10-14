1ffeba40845efc99b97bbcbbb5ce11dbbde985dc
tochisuke221 <tochisuke221@gmail.com>
2023-10-14 14:58:41 +0900

    useStateで十分な気がする

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useImmer } from "use-immer";
 
-function App() {
+
+
+const App = () => {
+  const [form, setForm] = useImmer({
+    name: '太郎',
+    address: {
+      prefecuture: '大阪府',
+      city: '池田市'
+    }
+  })
+
+  const handleForm = e => {
+    setForm(form => {
+      form[e.target.name] = e.target.value
+    })
+  }
+
+  const handleFormNest = e => {
+    setForm(form => {
+      form.address[e.target.name] = e.target.value;
+    });
+  };
+
+  const show = () => {
+    console.log(form)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <div>
+        <label htmlFor="name">名前：</label>
+        <input id="name" name="name" type="text"
+          onChange={handleForm} value={form.name} />
+      </div>
+      <div>
+        <label htmlFor="prefecture">住所（都道府県）：</label>
+        <input id="prefecture" name="prefecture" type="text"
+          onChange={handleFormNest} value={form.address.prefecture} />
+      </div>
+      <div>
+        <label htmlFor="city">住所（市町村）：</label>
+        <input id="city" name="city" type="text"
+          onChange={handleFormNest} value={form.address.city} />
+      </div>
+      <div>
+        <button type="button" onClick={show}>
+          送信</button>
+      </div>
+    </form>
   );
 }
 
-export default App;
+export default App
