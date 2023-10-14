2206d887b12c92c8a5bc2c32088f4ed8ba7ccd61
tochisuke221 <tochisuke221@gmail.com>
2023-10-15 00:48:22 +0900

    ドロワーメニューの途中

--- src/components/MaterialButton.js
+++ src/components/MaterialButton.js
@@ -1,9 +1,18 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react'
 import { Button } from '@mui/material'
 
 const MaterialButton = () => {
+    const font = css`
+      text-transform: none;
+    `
+
+
+
     return (
         <>
-          <Button variant="text">1</Button>
+          <Button variant="text" css={font}>1. Text</Button>
           <Button variant="contained">2</Button>
           <Button variant="outlined">3</Button>
         </>
