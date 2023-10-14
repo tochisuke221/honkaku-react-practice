36576e333641081c95ac4a23f639f994c011d500
tochisuke221 <tochisuke221@gmail.com>
2023-10-14 17:05:16 +0900

    ポータル

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Form from "./components/Form"; 
 import LazyBasic from "./components/LazyBasic";
 import ProfilerBasic from "./components/ProfilerBasic";
-
+import PortalBasic from "./components/PortalBasic"
 
 const App = () => {
   // return <Form />
-  return <ProfilerBasic />
+  // return <ProfilerBasic />
+  return (
+    <>
+      <div id="dialog"></div>
+      <PortalBasic />
+    </>
+  )
   // return <LazyBasic />
+
 }
 
 export default App
