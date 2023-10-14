335a46148de7e7147a2359300fba1d7004bcb0d0
tochisuke221 <tochisuke221@gmail.com>
2023-10-15 00:40:26 +0900

    MUI

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import Form from "./components/Form";
 import LazyBasic from "./components/LazyBasic";
 import ProfilerBasic from "./components/ProfilerBasic";
 import PortalBasic from "./components/PortalBasic"
+import MaterialButton from "./components/MaterialButton";
 
 const App = () => {
   // return <Form />
   // return <ProfilerBasic />
-  return (
-    <>
-      <div id="dialog"></div>
-      <PortalBasic />
-    </>
-  )
+  // return (
+  //   <>
+  //     <div id="dialog"></div>
+  //     <PortalBasic />
+  //   </>
+  // )
   // return <LazyBasic />
+  return <MaterialButton />
 
 }
 
---
+++ src/components/MaterialButton.js
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+
+const MaterialButton = () => {
+    return (
+        <>
+          <Button variant="text">1</Button>
+          <Button variant="contained">2</Button>
+          <Button variant="outlined">3</Button>
+        </>
+    )
+}
+
+export default MaterialButton
